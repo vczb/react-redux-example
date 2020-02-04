b2ce7a44999dbc753d516933b039f0163d2ecc96
Vinicius CFGroup <vz@cfgroup.com.br>
2020-02-03 21:35:06 -0300

    index layout

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.jpg';
+import './style.css';
 
 export default function Header(){
   return(
-    <header>
-      header
+    <header className="container" >
+      <Link to="/">
+        <img className="logo" src={logo} alt="Logo do Projeto" />
+      </Link>
+      <Link className="reserv" to="/reserv">
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>0 Reservas</span>
+        </div>
+      </Link>
     </header>
   );
 }
