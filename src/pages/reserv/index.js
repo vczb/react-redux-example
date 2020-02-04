554199d333e05418384344c71deacde2a0399c32
Vinicius CFGroup <vz@cfgroup.com.br>
2020-02-03 21:35:14 -0300

    reserv component

--- src/pages/reserv/index.js
+++ src/pages/reserv/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Reserved(){
+export default function Reserv(){
   return(
     <div>
-    Reserved page
+    Reservation page
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import Home from './pages/home';
-import Reserved from './pages/reserved';
+import Home   from './pages/home';
+import Reserv from './pages/reserv';
 
 export default function Routes(){
   return(
     <Switch>
       <Route path="/" exact component={Home}  />
-      <Route path="/reserved" exact component={Reserved}  />
+      <Route path="/reserv" exact component={Reserv}  />
     </Switch>
   );
 }
