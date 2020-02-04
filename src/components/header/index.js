36d7afc2e26d4f1e58b8a7c29f3da1d61fcad1ff
Vinicius CFGroup <vz@cfgroup.com.br>
2020-02-03 23:10:07 -0300

    home style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.jpg';
+import logo from '../../assets/logo.svg';
 import './style.css';
 
 export default function Header(){
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { MdFlightTakeoff } from 'react-icons/md';
+import api from '../../services/api';
+import  './style.css';
 
 export default function Home(){
+  const [trips, setTrips] = useState([]);
+
+  useEffect(()=>{
+
+    async function loadAPI(){
+      const res = await api.get('trips');
+      setTrips(res.data);
+
+      console.log(res.data);
+    }
+    loadAPI();
+  }, []);
+
   return(
     <div>
-    Home page
+      <div className="box" >
+        {trips.map(trip =>(
+          <li key={trip.id} >
+            <img src={trip.image} alt={trip.title}/>
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? 'Disponível' : 'Indisponível'}</span>
+            <button
+              onClick={()=>{}}
+              type="button">
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF"/>
+              </div>
+              <span>SOLICITAR RESERVA</span>
+            </button>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
